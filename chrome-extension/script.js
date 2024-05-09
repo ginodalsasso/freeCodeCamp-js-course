0ad145ecc012f435e140c871a8f4af79f3170e63
Dalsasso Gino <159161313+ginodalsasso@users.noreply.github.com>
2024-05-09 15:44:16 +0200

    finishing chrome extension

--- chrome-extension/script.js
+++ chrome-extension/script.js
@@ -1,21 +1,55 @@
 
-let myLeads = [1, 2, 3]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn= document.getElementById("input-btn")
-
 const ulEL = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
 
-inputBtn.addEventListener("click", function(){
-    //.value recupère la donnée inscrit dans l'input
-    myLeads.push(inputEl.value)
-    renderLeads()
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if(leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+tabBtn.addEventListener("click", function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
 })
 
-function renderLeads(){
+function render(leads){
     let listItems = ""
-    for(let i = 0; i < myLeads.length; i++){
-        listItems += "<li>" + myLeads[i] + "</li>"
+    for(let i = 0; i < leads.length; i++){
+        listItems +=//${ma variable} est un autre moyen d'echaper une variable sans +
+         `
+        <li>
+            <a target='_blank' href= '${leads[i]}' > 
+                ${leads[i]} 
+            </a>
+        </li>
+        `
     }
     
     ulEL.innerHTML = listItems
 }
+
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click", function(){
+    //.value recupère la donnée inscrit dans l'input
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+})
+
+
